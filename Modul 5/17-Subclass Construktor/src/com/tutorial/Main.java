82be4bdf0f18a89674c18c126d4a8fbbb9990026
azzam1342 <146051837+azzam1342@users.noreply.github.com>
2024-12-11 23:42:37 -0400

    Add files via upload

---
+++ Modul 5/17-Subclass Construktor/src/com/tutorial/Main.java
@@ -0,0 +1,8 @@
+package com.tutorial;
+
+public class Main {
+    public static void main(String[] args) {
+        HeroTank hero1 = new HeroTank("hilda", 1000, 200);
+        hero1.cetak();
+    }
+}
