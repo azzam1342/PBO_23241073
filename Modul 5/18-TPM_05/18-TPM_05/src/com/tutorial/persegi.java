794462d67b8289bb2a5263db01a70f0c3ce834ae
azzam1342 <146051837+azzam1342@users.noreply.github.com>
2024-12-20 21:59:46 +0800

    Add files via upload

---
+++ Modul 5/18-TPM_05/18-TPM_05/src/com/tutorial/persegi.java
@@ -0,0 +1,19 @@
+package com.tutorial;
+
+public class persegi extends BangunDatar {
+    private float sisi;
+
+    public persegi(float sisi) {
+        this.sisi = sisi;
+    }
+
+    //Override
+    public float luas() {
+        return sisi * sisi;
+    }
+
+    //Override
+    public float keliling() {
+        return 4 * sisi;
+    }
+}
